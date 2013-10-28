943459b095aec1303ebb75ffa0b6dcbdd38f579a
Lars-Åke Fredlund <lfredlund@fi.upm.es>
2013-10-28 17:01:34 +0100

    Fix

--- examples/trainDoors/system.erl
+++ examples/trainDoors/system.erl
@@ -8,14 +8,14 @@ start(NDoors) ->
   DriverButton = 'driverButton',
   register
     (DriverButton,
-     spawn_link(fun () -> process:start({driverButton,TCMS}) end)),
+     spawn_link(fun () -> process:start([{driverButton,TCMS}]) end)),
   Traction = 'traction',
   register
     (Traction,
-     spawn_link(fun () -> process:start({traction,void}) end)),
+     spawn_link(fun () -> process:start([{traction,void}]) end)),
   register
     (TCMS,
-     spawn_link(fun () -> process:start({tcms,{Doors,Traction,DriverButton}}) end)).
+     spawn_link(fun () -> process:start([{tcms,{Doors,Traction,DriverButton}}]) end)).
 
 create_doors(N,TCMS) ->
   lists:foreach
@@ -25,24 +25,24 @@ create_doors(N,TCMS) ->
 	 OpenSensor = list_to_atom("limitSensor_"++NS),
 	 register
 	   (OpenSensor,
-	    spawn_link(fun () -> process:start({limitSensor,Door}) end)),
+	    spawn_link(fun () -> process:start([{limitSensor,Door}]) end)),
 	 CloseSensor = list_to_atom("closeSensor_"++NS),
 	 register
 	   (CloseSensor,
-	    spawn_link(fun () -> process:start({closeSensor,Door}) end)),
-	 ObstacleSensor = list_to_atom("closeSensor_"++NS),
+	    spawn_link(fun () -> process:start([{closeSensor,Door}]) end)),
+	 ObstacleSensor = list_to_atom("obstacleSensor_"++NS),
 	 register
 	   (ObstacleSensor,
-	    spawn_link(fun () -> process:start({obstacleSensor,Door}) end)),
+	    spawn_link(fun () -> process:start([{obstacleSensor,Door}]) end)),
 	 DoorButton = list_to_atom("dootButton_"++NS),
 	 register
 	   (DoorButton,
-	    spawn_link(fun () -> process:start({doorButton,Door}) end)),
+	    spawn_link(fun () -> process:start([{doorButton,Door}]) end)),
 	 register
 	   (Door,
 	    spawn_link
 	      (fun () ->
-		   process:start({door,{TCMS, OpenSensor, CloseSensor, ObstacleSensor}})
+		   process:start([{door,{TCMS, OpenSensor, CloseSensor, ObstacleSensor}}])
 	       end))
      end, lists:seq(1,N)).
 
