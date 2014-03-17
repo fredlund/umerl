fb7a7df4738225aa3130c69f6a16049d4d337434
Lars-Åke Fredlund <lfredlund@fi.upm.es>
2014-03-17 13:26:13 +0100

    Documentation fixes.

--- examples/table.erl
+++ examples/table.erl
@@ -29,22 +29,12 @@
 -compile(export_all).
 
 table(N) ->
-  Forks = lists:map (fun (_) -> start_fork() end, lists:seq(1,N)),
+  Forks = lists:map (fun (_) -> process:start([{fork,void}]) end, lists:seq(1,N)),
   lists:foreach
-    (fun ({L,R}) -> start_philosopher(L, R) end, adjacent(Forks)).
+    (fun ({L,R}) -> process:start([{philosopher,{L,R}}]) end, adjacent(Forks)).
 
 adjacent([]) -> [];
 adjacent([X|Xs]) -> lists:zip([X] ++ Xs, Xs ++ [X]).
 
-start_fork() ->
-  spawn(fun () -> process:start([{fork,void}]) end).
-
-start_philosopher(L,R) ->
-  spawn(fun () -> process:start([{philosopher,{L,R}}]) end).
-
-test_table() ->
-  Left = start_fork(),
-  Right = start_fork(),
-  start_philosopher(Left,Right).
 
   
